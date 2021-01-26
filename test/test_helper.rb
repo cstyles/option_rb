d2aa9066b9650cac30e59adda0b5eda10136b9df
Collin Styles <collingstyles@gmail.com>
2021-01-25 21:44:28 -0800

    Set up test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'option_rb'
+require 'option_rb/prelude'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+require 'mocha/minitest'
+require 'shoulda-context'
+
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new

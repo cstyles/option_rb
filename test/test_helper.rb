62989a440e7a46d0efa5620ee05a9d884e98c5d8
Collin Styles <collingstyles@gmail.com>
2021-01-25 22:26:06 -0800

    Collect code coverage data when `MEASURE_COVERAGE` is set

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+if ENV['MEASURE_COVERAGE']
+  require 'simplecov'
+
+  SimpleCov.start do
+    enable_coverage :branch
+    # minimum_coverage line: 100, branch: 100
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'option_rb/prelude'
 
